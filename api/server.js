b58abf3a4524c6af1f3cdc8557b18c96edf48ff8
Nidrux <originalfroaz@gmail.com>
2022-10-11 21:00:31 +0200

    Added PORT to enviroment variable

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
 const express = require('express');
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "../client/build")))
 // handle settings router.
 const settingsRouter = require("./routes/settings.js");
 app.use("/settings", settingsRouter)
-
 app.listen(PORT, (error) => {
     if(error) throw error;
     console.log(`listening on port ${PORT}`)
